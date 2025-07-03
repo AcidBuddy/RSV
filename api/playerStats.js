e30298d52bb3d607847dd2e31d8402617710eede
AcidBuddy <144478921+AcidBuddy@users.noreply.github.com>
2025-07-03 23:57:57 +0200

    Add files via upload

--- api/playerStats.js
+++ api/playerStats.js
@@ -2,7 +2,7 @@ import API from '@callofduty/api';
 
 export default async function handler(req, res) {
   const CallOfDutyAPI = new API();
-  const { xsrf, sso, atkn } = await CallOfDutyAPI.Authorize('your@email.com', 'yourPassword');
+  const { xsrf, sso, atkn } = await CallOfDutyAPI.Authorize('acidbuddyspam@gmail.com', 'kieperkieFOKKER1@');
   CallOfDutyAPI.UseTokens({ xsrf, sso, atkn });
 
   const profile = await CallOfDutyAPI.Profile(

59aeab3c71f5101708c58f41eeb650960c9a9501
AcidBuddy <144478921+AcidBuddy@users.noreply.github.com>
2025-07-04 00:08:18 +0200

    Update playerStats.js

--- api/playerStats.js
+++ api/playerStats.js
@@ -2,8 +2,10 @@ import API from '@callofduty/api';
 
 export default async function handler(req, res) {
   const CallOfDutyAPI = new API();
-  const { xsrf, sso, atkn } = await CallOfDutyAPI.Authorize('acidbuddyspam@gmail.com', 'kieperkieFOKKER1@');
-  CallOfDutyAPI.UseTokens({ xsrf, sso, atkn });
+  const { xsrf, sso, atkn } = await CallOfDutyAPI.Authorize(
+  process.env.ACTIVISION_EMAIL,
+  process.env.ACTIVISION_PASSWORD
+);
 
   const profile = await CallOfDutyAPI.Profile(
     { username: 'AcidBuddy#2161045', platform: 'uno' },
